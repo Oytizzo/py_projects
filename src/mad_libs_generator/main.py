25d1d82fe1068451d97e078f26fa92f99e7ccb87
Oytizzo <oytizzo@gmail.com>
2021-04-14 21:23:47 +0600

    Word count added

---
+++ src/mad_libs_generator/main.py
@@ -0,0 +1,46 @@
+count = 1
+
+while count < 10:
+    male_name = input("Choose a male name: ")                           # Rory
+    fav_teacher_name = input("Name of your favourite teacher: ")        # Mrs. tat
+    excamation = input("An exclamation word: ")                         # crums
+    number = input("A number: ")                                        # 4229
+    plural_obj = input("Give us a plural object: ")                     # eggs
+    store_name = input("Store name: ")                                  # macy's
+    body_part = input("A body part: ")                                  # buttock
+    silly_word = input("A silly word: ")                                # flibity zibit
+    holyday_name = input("Name of a hollyday: ")                        # henyka
+    movie_title = input("Movie title: ")                                # superman
+    verb_in_ing = input("Verb ending in ing: ")                         # zirating
+    distance = input("ammount of distance: ")                           # half a milimeter
+    country = input("Country: ")                                        # ukraine
+    animal = input("Animal: ")                                          # badger
+    fam_movie_quote = input("Famous movie quote: ")                     # this town needs an enema
+    another_body_part = input("Another body part: ")                    # little toe
+    children_song = input("Children's song: ")                          # ba ba black sheep
+    adjective = input("Adjective: ")                                    # mind bending
+
+    # Displays the story based on the user input
+    print("-------------------------------------------------------")
+    print("Hello!")
+    print("person 1: I'm detective", male_name, "and you are?")
+    print("person 2:", fav_teacher_name)
+    print("person 1: You are here to under suspesous second degree robary.")
+    print("person 2:", excamation)
+    print("person 1: That's right.", number, plural_obj, "were stolen from", store_name, 
+        "in the crime scene your", body_part, "were written all over it.")
+    print("person 2:", silly_word)
+    print("person 1: Where were you in the night of", holyday_name, "?")
+    print("person 2: We were watching", movie_title)
+    print("person 1: Then why does security camera footage showed you", 
+        verb_in_ing, "just", distance, "away from the crime scene.")
+    print("person 1: Alright. I'm through with playing games. Where are you from?")
+    print("person 2:", country)
+    print("person 1: Just as i suspected. You know one of the best parts of being a detective is that i get to lock up criminals like you. And home to my children and my pet", 
+        animal, "and say", fam_movie_quote)
+    print("person 2: Fine! i did it. I commited the robary. I did it for the money. To buy myself a", another_body_part, "implant")
+    print("person 1: I knew it all along! I knew it. And every time i solve a crime, i like to sing my favourite song,", children_song)
+    print("person 2: You have a", adjective, "voice.")
+    print("----------------------------------------------------------")
+
+    count += 1
---
+++ src/word_count/main.py
@@ -0,0 +1,6 @@
+sentence = input("What's on your mind today?\n")
+list_of_words_in_that_sentence = sentence.split()
+
+word_count = len(list_of_words_in_that_sentence)
+
+print(word_count)
