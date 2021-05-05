f3f56365f887893068a08b78fcae23b9a44db523
Oytizzo <oytizzo@gmail.com>
2021-05-05 17:51:04 +0600

    load and dump

--- src/json_working/json_ex.py
+++ src/json_working/json_ex.py
@@ -1,31 +1,11 @@
 ''' JavaScript object Notation '''
 import json
 
-people_string = '''
-{
-    "people": [
-        {
-            "name": "John Smith",
-            "phone": "123-456-789",
-            "emails": ["johnsmith@example.com", "john.smith@example.com"],
-            "has_license": false
-        },
-        {
-            "name": "Jane Doe",
-            "phone": "456-789-126",
-            "emails": null,
-            "has_license": true
-        }
-    ]
-}
-'''
+with open('states.json') as f:
+    data = json.load(f)
 
-data = json.loads(people_string)
+for state in data['states']:
+    del state['area_codes']
 
-for person in data['people']:
-    del person['phone']
-
-new_string = json.dumps(data, indent=2, sort_keys=True)
-
-print(new_string)
-print(type(new_string))
+with open('new_states.json', 'w') as f:
+    json.dump(data, f, indent=2)
