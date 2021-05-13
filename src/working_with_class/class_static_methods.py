52095dc4dd3d4f8f20c388a48345eea5fc6e1cf0
Oytizzo <oytizzo@gmail.com>
2021-05-13 17:44:59 +0600

    Added Dunders

--- src/working_with_class/class_static_methods.py
+++ src/working_with_class/class_static_methods.py
@@ -8,67 +8,45 @@ class Employee:
         self.email = first + '.' + last + '@email.com'
         self.pay = pay
 
-
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
     
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
     
-    @classmethod
-    def set_raise_amt(cls, amount):
-        cls.raise_amt = amount
+    def __repr__(self):
+        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
 
-    @classmethod
-    def from_string(cls, emp_str):
-        first, last, pay = emp_str.split('-')
-        return cls(first, last, pay)
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
 
-    @staticmethod
-    def is_workday(day):
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
+    def __add__(self, other):
+        return self.pay + other.pay
 
+    def __len__(self):
+        return len(self.fullname())
 
-class Developer(Employee):
-    raise_amt = 1.10
+emp_1 = Employee('Jane', 'Doe', 50000)
+emp_2 = Employee('David', 'John', 40000)
 
-    def __init__(self, first, last, pay, prog_lang):
-        super().__init__(first, last, pay)
-        self.prog_lang = prog_lang
+# print(emp_1)
 
+# print(repr(emp_1))
+# print(str(emp_1))
 
-class Manager(Employee):
+# print(emp_1.__repr__())
+# print(emp_1.__str__())
 
-    def __init__(self, first, last, pay, employees=None):
-        super().__init__(first, last, pay)
-        if employees is None:
-            self.employees = []
-        else:
-            self.employees = employees
-    
-    def add_emp(self, emp):
-        if emp not in self.employees:
-            self.employees.append(emp)
-    
-    def remove_emp(self, emp):
-        if emp in self.employees:
-            self.employees.remove(emp)
-    
-    def print_emps(self):
-        for emp in self.employees:
-            print('-->', emp.fullname())
+# print(1+2)
+# print(int.__add__(1, 2))
 
-dev_1 = Developer('Jane', 'Doe', 50000, 'Python')
-dev_2 = Developer('David', 'John', 60000, 'C')
+# print(str.__add__('a', 'b'))
 
-mgr_1 = Manager("Lin", "Yung", 100000, [dev_1])
+# print(emp_1+emp_2)
+# print(Employee.__add__(emp_1, emp_2))
 
-print(isinstance(mgr_1, Manager))
-print(isinstance(mgr_1, Employee))
-print(isinstance(mgr_1, Developer))
+# print(len('Jane'))
+# print('Jane'.__len__())
 
-print(issubclass(Developer, Employee))
-print(issubclass(Manager, Employee))
-print(issubclass(Manager, Developer))
+print(len(emp_1))
+print(len(emp_2))
