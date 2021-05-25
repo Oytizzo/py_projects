35529dc1bfe62dec9e23b2d274599e075a963ed8
Oytizzo <oytizzo@gmail.com>
2021-05-25 22:05:43 +0600

    Multi-level inheritance abbuse

--- src/class_test/app2.py
+++ src/class_test/app2.py
@@ -1,29 +1,17 @@
 class Animal:
-    def __init__(self) -> None:
-        print("Animal Constructor")
-        self.age = 1
-
     def eat(self):
         print("eat")
 
 
-class Mammal(Animal):
-    def __init__(self) -> None:
-        # super().__init__()
-        print("Mammal Constructor")
-        self.weight = 2
-        super().__init__()
-
-    def walk(self):
-        print("walk")
+class Bird(Animal):
+    def fly(self):
+        print("fly")
 
 
-class Fish(Animal):
-    def swim(self):
-        print("swim")
+class Chicken(Bird):
+    pass
 
-
-# type m.    and see the magic
-m = Mammal()
-print(m.age)
-print(m.weight)
+c = Chicken()
+# chicken cannot fly
+c.fly()
+c.eat()
