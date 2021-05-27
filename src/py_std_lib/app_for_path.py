0c15a292cad671b9b516ee5234ac576561223260
Oytizzo <oytizzo@gmail.com>
2021-05-27 23:19:13 +0600

    Add files functions

--- src/py_std_lib/app_for_path.py
+++ src/py_std_lib/app_for_path.py
@@ -1,16 +1,20 @@
 from pathlib import Path
+from time import ctime
+import shutil
 
-print(Path.home())
-path = Path(r"py_std_lib\app_for_path.py")
-print(path.exists())
-path.is_file()
-path.is_dir()
-print(path.name)
-print(path.stem)
-print(path.suffix)
-print(path.parent)
-path = path.with_name("file.txt")
-print(path)
-print(path.absolute())
-path = path.with_suffix(".txt")
-print(path)
+source = Path(r"py_std_lib\app_for_path.py")
+target = Path() / "__init__.py"
+
+shutil.copy(source, target)
+
+# print(Path.home())
+# path = Path(r"py_std_lib\app_for_path.py")
+# path.exists()
+# path.rename("app.txt")
+# path.unlink()
+# print(ctime(path.stat().st_ctime))
+
+# path.read_bytes()
+# path.read_text()
+# path.write_text("....")
+# path.write_bytes()
