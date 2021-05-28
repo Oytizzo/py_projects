d688b74a62a1bd9990d631449acaf02f1d7eee18
Oytizzo <oytizzo@gmail.com>
2021-05-28 22:18:03 +0600

    Parse questions and votes

--- src/web_scrapping/app.py
+++ src/web_scrapping/app.py
@@ -10,3 +10,4 @@ questions = soup.select(".question-summary")
 
 for question in questions:
     print(question.select_one(".question-hyperlink").getText())
+    print(question.select_one(".vote-count-post").getText())
