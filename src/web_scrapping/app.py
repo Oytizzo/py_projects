66c199591451f603f09f77c300d2ef60922da9e3
Oytizzo <oytizzo@gmail.com>
2021-05-28 22:09:50 +0600

    Parse all question in dict

--- src/web_scrapping/app.py
+++ src/web_scrapping/app.py
@@ -4,5 +4,10 @@ from bs4 import BeautifulSoup
 url = "https://stackoverflow.com/questions"
 response = requests.get(url)
 
-print(response.reason)
-print(response.status_code)
+soup = BeautifulSoup(response.text, "html.parser")
+
+questions = soup.select(".question-summary")
+
+print(type(questions[0]))
+print(type(questions[0].attrs))
+print(questions[0].attrs)
