690f3afd44947b1e91ed8463974ed34182145793
Oytizzo <oytizzo@gmail.com>
2021-06-01 23:29:56 +0600

    Add parse_csv.py

---
+++ src/csv/parse_csv.py
@@ -0,0 +1,12 @@
+import csv
+
+with open('names.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    # print(csv_reader)
+    
+    count = 0
+    for line in csv_reader:
+        print(line)
+        count += 1
+    print(count)
