f4c9508f242a04cc89db21f8ba9bdfad3eee4055
Oytizzo <oytizzo@gmail.com>
2021-06-01 23:40:07 +0600

    Add csv writer

--- src/csv/parse_csv.py
+++ src/csv/parse_csv.py
@@ -2,11 +2,10 @@ import csv
 
 with open('names.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
-
     # print(csv_reader)
-    next(csv_reader)
-    count = 0
-    for line in csv_reader:
-        print(line)
-        count += 1
-    print(count)
+
+    with open('new_names.csv', 'w') as new_file:
+        csv_writer = csv.writer(new_file, delimiter='-')
+
+        for line in csv_reader:
+            csv_writer.writerow(line)
