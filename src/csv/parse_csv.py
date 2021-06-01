3549f7136aef6bc52d81b3f457c7204054993e1b
Oytizzo <oytizzo@gmail.com>
2021-06-01 23:45:51 +0600

    Add \t as delimiter for csv writer

--- src/csv/parse_csv.py
+++ src/csv/parse_csv.py
@@ -5,7 +5,7 @@ with open('names.csv', 'r') as csv_file:
     # print(csv_reader)
 
     with open('new_names.csv', 'w', newline='') as new_file:
-        csv_writer = csv.writer(new_file, delimiter='-')
+        csv_writer = csv.writer(new_file, delimiter='\t')
 
         for line in csv_reader:
             csv_writer.writerow(line)
