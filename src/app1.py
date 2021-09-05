4b933d6b95b37a7a8981ec96c682c89592de67ed
Oytizzo <oytizzo@gmail.com>
2021-09-05 16:12:54 +0600

    turtle fib

--- src/app1.py
+++ src/app1.py
@@ -1,9 +1,16 @@
-def multiply(*numbers):
-    total = 1
-    for number in numbers:
-        total *= number
-    return total
+# def multiply(*numbers):
+#     total = 1
+#     for number in numbers:
+#         total *= number
+#     return total
+#
+#
+# total = multiply(2, 3, 4, 5)
+# print(total)
 
-
-total = multiply(2, 3, 4, 5)
-print(total)
+# import turtle
+# r = [1, 1, 2, 3, 5, 8, 13]
+# a = 90
+# for i in r:
+#     turtle.circle(i*10, a)
+# turtle.exitonclick()
